4fbcfb1b045134f5b512d5bb69e383527d778456
matsura <eldar32@gmail.com>
2016-05-08 08:20:32 +0200

    Test

--- testmatlab.php
+++ testmatlab.php
@@ -1,6 +1,6 @@
 <?php
 $scriptCommand = '"printString("x"); quit;"';
-$command = "./home/eldar/Desktop/MatlabInstall/matlab -r -nodisplay " . $scriptCommand; 
+$command = "./home/eldar/Desktop/MatlabInstall/bin/matlab -r -nodisplay " . $scriptCommand; 
 $output; $retval; $errors="";
 exec ($command ,  $output, $retval);
 echo $output[0] ."\n";
