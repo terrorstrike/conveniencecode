e39ac6c90a8088378ca7bfde6fedebff16f5cb67
matsura <eldar32@gmail.com>
2016-05-08 22:29:22 +0200

    Fixes

--- model/SpirometryResult.php
+++ model/SpirometryResult.php
@@ -19,69 +19,69 @@ class SpirometryResult {
 
    // Getters
 
-   function getVcIN() {
+   public function getVcIN() {
    	 return $this->$vcIN;
    }
 
-   function getFvc() {
+   public  function getFvc() {
    	 return $this->$fvc;
    }
 
-   function getFev1() {
+   public function getFev1() {
    	 return $this->$fev1;
    }
 
-   function getFev1VCMax() {
+   public function getFev1VCMax() {
    	 return $this->$fev1VCMax;
    }
 
-   function getPef() {
+   public function getPef() {
    	 return $this->$PEF;
    }
 
-   function getMEF75() {
+   public function getMEF75() {
    	 return $this->MEF75;
    }
 
-   function getMEF50() {
+   public function getMEF50() {
    	 return $this->MEF50;
    }
 
-   function getMEF25() {
+   public function getMEF25() {
    	 return $this->MEF25;
    }
 
    // Setters
    
-   function setVcIN($vcIN) {
+   public function setVcIN($vcIN) {
    	 $this->$vcIN = $vcIN;
    }
 
-   function setFvc($fvc) {
+   public function setFvc($fvc) {
    	 $this->$fvc = $fvc;
    }
 
-   function setFev1($fev1) {
+   public function setFev1($fev1) {
    	 $this->$fev1 = $fev1;
    }
 
-   function setFev1VCMax($fev1VCMax) {
+   public function setFev1VCMax($fev1VCMax) {
    	 $this->$fev1VCMax = $fev1VCMax;
    }
 
-   function setPef($PEF) {
+   public function setPef($PEF) {
    	 $this->$PEF = $PEF;
    }
 
-   function setMEF75($MEF75) {
+   public function setMEF75($MEF75) {
    	 $this->MEF75 = $MEF75;
    }
 
-   function setMEF50($MEF50) {
+   public function setMEF50($MEF50) {
    	 $this->MEF50 = $MEF50;
    }
 
-   function setMEF25($MEF25) {
+   public function setMEF25($MEF25) {
    	 $this->MEF25 = $MEF25;
    }
 
