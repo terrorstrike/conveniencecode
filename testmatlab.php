1b215c99f3a1cf1cf4f40a6d1f46d48a8c475b04
matsura <eldar32@gmail.com>
2016-05-08 08:16:07 +0200

    Test

--- testmatlab.php
+++ testmatlab.php
@@ -4,7 +4,7 @@ $cdRetVal;
 $cdCommand = "cd /home/eldar/Desktop/MatlabInstall/bin";
 exec($cdCommand, $cdOutput, $cdRetVal);
 $scriptCommand = '"printString("x"); quit;"';
-$command = "matlab -wait -r -nodisplay " . $scriptCommand; 
+$command = "./matlab -r -nodisplay " . $scriptCommand; 
 $output; $retval; $errors="";
 exec ($command ,  $output, $retval);
 echo $output[0] ."\n";
