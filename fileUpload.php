fd0c75537b895e2193d3abfe0f2c1b8c0c392e49
matsura <eldar32@gmail.com>
2016-05-13 11:42:32 +0200

    Testing regex

--- fileUpload.php
+++ fileUpload.php
@@ -29,10 +29,7 @@ try {
         $response['error'] = true;
         $response['message'] = 'Could not move the file!';
     } else {
-        // File successfully uploaded
-        $response['message'] = 'File uploaded successfully!';
-        $response['error'] = false;
-        
+
         // Get the contents of the pdf as a string
         $output = "";
         $pdfToTextCmd = "pdftotext -layout '" . $target_path . "' testText";
@@ -44,81 +41,85 @@ try {
         //$parts = preg_split($delimiterPattern, $contents);
         $parts = explode('\n', $contents);
         $index = -1;
-        //$partsTable = explode("\n", substr($contents, strpos($contents, $TABLE_BEGIN_INDEX_STRING)));
         if(preg_match("/ *Pred +Act1 +% +Soll/", $contents, $matches, PREG_OFFSET_CAPTURE)) {
             $index = $matches[0][1];
-            var_dump($index);
-            var_dump($matches);
-        }
-        // Prepare for parsing
-        $parts_array = array();
-        $offset = 8;
-        for($i = 0; $i < count($partsTable); $i++) {
-             if (strpos($partsTable[$i], $TABLE_BEGIN_INDEX_STRING) !== false ) {
-                 $index = $i;
-                 $break;
-             }
-        } 
+            $partsTable = explode("\n", substr($contents, $index));
+            // Prepare for parsing
+            $parts_array = array();
+            $offset = 8;
+            for($i = 0; $i < count($partsTable); $i++) {
+                 if (strpos($partsTable[$i], $TABLE_BEGIN_INDEX_STRING) !== false ) {
+                     $index = $i;
+                     $break;
+                 }
+            } 
 
-        for($j = $index + 1; $j <= $offset; $j++){
-             array_push($parts_array, $partsTable[$j]);
-        }
+            for($j = $index + 1; $j <= $offset; $j++){
+                 array_push($parts_array, $partsTable[$j]);
+            }
 
-        // $parts_array now contains measurement table which should now be parsed
-        $result = []; 
-        for($k = 0; $k < count($parts_array); $k++) {
-             // replace everything with 1+ space with one space and explode into array
-             $tmp = explode(' ', preg_replace('/ +/', ' ', $parts_array[$k]));
-             // put found measurements in an object
-             $measurement = $tmp[0];
-             $count = count($tmp);
-             // get part of table that has measurements
-             $slice = array_slice($tmp, $count - 3);             
-             if ($measurement === 'VC') {
-                 $result['VcIN'] = $slice;
-             } else if ($measurement === 'FEV') {
-                if ($count > 7) {
-                   $result['Fev1VcMax'] = $slice;
-                } else {
-                   $result['Fev1'] = $slice;
-                }
-             } else if ($measurement === 'FVC') {
-                $result['FVC'] = $slice;
-             } else if ($measurement === 'MEF') {
-                $key = $tmp[0] . $tmp[1];                
-                $result[$key] = $slice;
-             } else if ($measurement === 'PEF') {
-                $result['PEF'] = $slice;
-             }
-        }
+            // $parts_array now contains measurement table which should now be parsed
+            $result = []; 
+            for($k = 0; $k < count($parts_array); $k++) {
+                 // replace everything with 1+ space with one space and explode into array
+                 $tmp = explode(' ', preg_replace('/ +/', ' ', $parts_array[$k]));
+                 // put found measurements in an object
+                 $measurement = $tmp[0];
+                 $count = count($tmp);
+                 // get part of table that has measurements
+                 $slice = array_slice($tmp, $count - 3);             
+                 if ($measurement === 'VC') {
+                     $result['VcIN'] = $slice;
+                 } else if ($measurement === 'FEV') {
+                    if ($count > 7) {
+                       $result['Fev1VcMax'] = $slice;
+                    } else {
+                       $result['Fev1'] = $slice;
+                    }
+                 } else if ($measurement === 'FVC') {
+                    $result['FVC'] = $slice;
+                 } else if ($measurement === 'MEF') {
+                    $key = $tmp[0] . $tmp[1];                
+                    $result[$key] = $slice;
+                 } else if ($measurement === 'PEF') {
+                    $result['PEF'] = $slice;
+                 }
+            }
 
-        // normalize values
-        $result['FVC'] = (floatval($result['FVC'][2])) > 100 ? 100 : floatval($result['FVC'][2]);
-        $result['Fev1'] = (floatval($result['Fev1'][2])) > 100 ? 100 : floatval($result['Fev1'][2]);
-        $result['Fev1FVC'] = (floatval($result['Fev1']) / floatval($result['FVC'])) > 1 ? 1 : (floatval($result['Fev1']) / floatval($result['FVC']));
-        $result['PEF'] = (floatval($result['PEF'][2])) > 100 ? 100 : (floatval($result['PEF'][2]));
-        // execute matlab commands
-        // 
-        $inputVector = "[" . $result['FVC'] . "," . $result['Fev1'] . "," . $result['Fev1FVC'] . "," . $result['PEF'] . "]";
+            // normalize values
+            $result['FVC'] = (floatval($result['FVC'][2])) > 100 ? 100 : floatval($result['FVC'][2]);
+            $result['Fev1'] = (floatval($result['Fev1'][2])) > 100 ? 100 : floatval($result['Fev1'][2]);
+            $result['Fev1FVC'] = (floatval($result['Fev1']) / floatval($result['FVC'])) > 1 ? 1 : (floatval($result['Fev1']) / floatval($result['FVC']));
+            $result['PEF'] = (floatval($result['PEF'][2])) > 100 ? 100 : (floatval($result['PEF'][2]));
+            // execute matlab commands
+            // 
+            $inputVector = "[" . $result['FVC'] . "," . $result['Fev1'] . "," . $result['Fev1FVC'] . "," . $result['PEF'] . "]";
 
-        $matlabCommand = 'echo apach3T3mp | /usr/bin/sudo -S /home/eldar/Desktop/MatlabInstall/bin/glnxa64/MATLAB -r -nodisplay "SPIR_Fuzzy=readfis(' . "'SPIR-Fuzzy');value=evalfis(" . $inputVector . ",SPIR_Fuzzy);disp(value);" . '"';
-        $cmdOutput = "";
-        exec($matlabCommand, $cmdOutput);
-        var_dump($cmdOutput);
-        if (isset($cmdOutput) && !empty($cmdOutput)) {
-            $value = floatval(trim($cmdOutput[10]));
-            var_dump($value);
+            $matlabCommand = 'echo apach3T3mp | /usr/bin/sudo -S /home/eldar/Desktop/MatlabInstall/bin/glnxa64/MATLAB -r -nodisplay "SPIR_Fuzzy=readfis(' . "'SPIR-Fuzzy');value=evalfis(" . $inputVector . ",SPIR_Fuzzy);disp(value);" . '"';
+            $cmdOutput = "";
+            exec($matlabCommand, $cmdOutput);
+            var_dump($cmdOutput);
+            if (isset($cmdOutput) && !empty($cmdOutput)) {
+                $value = floatval(trim($cmdOutput[10]));
+                var_dump($value);
 
-            $diagnose_value = 4;
+                $diagnose_value = 4;
 
-            if (abs($value - 0.1) < $EPS) $diagnose_value = 1;
-            if (abs($value - 0.35) < $EPS) $diagnose_value = 2;
-            if (abs($value - 0.6) < $EPS) $diagnose_value = 3;
-            if (abs($value - 0.85) < $EPS) $diagnose_value = 4;
-            var_dump($diagnose_value);
-            $state = $DIAGNOSIS_VALUES[$diagnose_value - 1];
-            // send email report
-            send_email('spirometry-results@spirometry.ba', 'eldar32@gmail.com', 'Your results', 'The state of your lungs is: ' . $state );
+                if (abs($value - 0.1) < $EPS) $diagnose_value = 1;
+                if (abs($value - 0.35) < $EPS) $diagnose_value = 2;
+                if (abs($value - 0.6) < $EPS) $diagnose_value = 3;
+                if (abs($value - 0.85) < $EPS) $diagnose_value = 4;
+                var_dump($diagnose_value);
+                $state = $DIAGNOSIS_VALUES[$diagnose_value - 1];
+                // send email report
+                send_email('spirometry-results@spirometry.ba', 'eldar32@gmail.com', 'Your results', 'The state of your lungs is: ' . $state );
+                // File successfully uploaded
+                $response['message'] = 'Successfully parsed the report, check your e - mail!';
+                $response['error'] = false;
+            }
+        } else {
+            $response['error'] = true;
+            $response['message'] = 'Invalid file sent!';
         }
         
     }
