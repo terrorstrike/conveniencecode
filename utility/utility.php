779696e02b09eeae87b964fe4da71ec27bbc2c48
matsura <eldar32@gmail.com>
2016-05-13 12:12:08 +0200

    Testing output

--- utility/utility.php
+++ utility/utility.php
@@ -3,7 +3,9 @@
 include('/var/www/file_upload/utility/constants.php');
 
 function send_email($from, $to, $subject, $text) {
-    exec("curl -s --user '" . "api:key-2cf78ac777f8bbb038017d7dd35104b3"  . "' \ " . " https://api.mailgun.net/v3/sandbox2904d7336bb443559e446612dbc8f616.mailgun.org/messages "  . " \ -F from='" . $from . "' \ -F to='" . $to . "' \ -F subject='" . $subject . "' \ -F text='" . $text . "'");
+	$output = "";
+    exec("curl -s --user '" . "api:key-2cf78ac777f8bbb038017d7dd35104b3"  . "' \ " . " https://api.mailgun.net/v3/sandbox2904d7336bb443559e446612dbc8f616.mailgun.org/messages "  . " \ -F from='" . $from . "' \ -F to='" . $to . "' \ -F subject='" . $subject . "' \ -F text='" . $text . "'", $output);
+    var_dump($output);
 }
 
 ?>
